53de98069091f10c31ae3248c817703e086f74d1
Sam-rst <samuel.ressiot@gmail.com>
2023-04-06 18:21:22 +0200

    Tiled

---
+++ graphics/Tiled/data/tsx/Overworld_tiled.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../background/Overworld.png" width="640" height="576"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="Grass" color="#3abe41" tile="-1" probability="1"/>
+   <wangcolor name="Water" color="#1e7cb8" tile="-1" probability="1"/>
+   <wangcolor name="Sand" color="#e0f878" tile="-1" probability="1"/>
+   <wangcolor name="Bois" color="#ff7700" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="40" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="41" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="42" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="43" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="80" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="81" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="82" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="83" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="120" wangid="0,1,0,3,0,1,0,1"/>
+   <wangtile tileid="121" wangid="0,1,0,3,0,3,0,1"/>
+   <wangtile tileid="122" wangid="0,1,0,1,0,3,0,1"/>
+   <wangtile tileid="123" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="124" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="125" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="160" wangid="0,3,0,3,0,1,0,1"/>
+   <wangtile tileid="161" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="162" wangid="0,1,0,1,0,3,0,3"/>
+   <wangtile tileid="163" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="164" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="165" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="200" wangid="0,3,0,1,0,1,0,1"/>
+   <wangtile tileid="201" wangid="0,3,0,1,0,1,0,3"/>
+   <wangtile tileid="202" wangid="0,1,0,1,0,1,0,3"/>
+   <wangtile tileid="203" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="204" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="205" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="240" wangid="0,3,0,1,0,3,0,3"/>
+   <wangtile tileid="241" wangid="0,3,0,3,0,1,0,3"/>
+   <wangtile tileid="242" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="243" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="244" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="280" wangid="0,1,0,3,0,3,0,3"/>
+   <wangtile tileid="281" wangid="0,3,0,3,0,3,0,1"/>
+   <wangtile tileid="282" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="283" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="284" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="322" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="323" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="324" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="360" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="361" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="362" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="363" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="364" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="365" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="366" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="400" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="401" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="402" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="403" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="404" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="405" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="406" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="528" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="529" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="530" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="531" wangid="0,2,0,0,0,0,0,2"/>
+   <wangtile tileid="568" wangid="0,0,0,0,0,2,0,2"/>
+   <wangtile tileid="569" wangid="0,2,0,2,0,0,0,0"/>
+   <wangtile tileid="570" wangid="0,0,0,2,0,2,0,0"/>
+   <wangtile tileid="571" wangid="0,0,0,2,0,2,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
