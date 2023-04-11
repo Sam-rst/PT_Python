ace6a7d5347a4bdd8f682d24e3d5bf2fa9d5abfd
Sam-rst <samuel.ressiot@gmail.com>
2023-04-11 10:57:25 +0200

    test

--- obstacle.py
+++ obstacle.py
@@ -8,3 +8,4 @@ class Obstacle(pygame.sprite.Sprite):
         # self.rect = pygame.Rect(collision_object.x, collision_object.y, collision_object.width, collision_object.height)
         self.rect = self.image.get_rect(center = (collision_object.x, collision_object.y))
         self.old_rect = self.rect.copy()
+        
